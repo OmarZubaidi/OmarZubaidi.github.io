abb3ebac5f5c2cbfa22fe836db32cf93fe0c66d2
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2025-09-18 22:34:24 -0400

    remove react router mock

--- .storybook/decorators/withRouter.tsx
+++ .storybook/decorators/withRouter.tsx
@@ -1,13 +1,7 @@
 import type { Decorator } from '@storybook/react-vite';
-import * as reactRouterModule from 'react-router';
 import { BrowserRouter } from 'react-router';
 
 export const withRouter: Decorator = (Story, context) => {
-  const mockReactRouter = context.parameters?.mockReactRouter;
-  if (mockReactRouter) {
-    Object.assign(reactRouterModule, mockReactRouter);
-  }
-
   return (
     <BrowserRouter>
       <Story />
